f6d65b4ef100eb748936a6b9899581be943d274b
Song Lee <songlish233@gmail.com>
2021-09-22 17:45:54 +0900

    feat: [#6] 캘린더 기본 메소드 작성, 필요한 엔티티 생성
    - dateKey를 만들 Date Formatter 역할 객체 필요

--- booklendar/booklendar/Model/Calendar.swift
+++ booklendar/booklendar/Model/Calendar.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+class Calendar {
+    private var days: [Int: Day]
+    
+    init(days: [Int: Day]) {
+        self.days = days
+    }
+    
+    func mainBook(for dateKey: Int) -> Record? {
+        guard let day = days[dateKey] else {
+            return nil
+        }
+        return day.main()
+    }
+    
+    func allBooks(for dateKey: Int) -> [Record]? {
+        guard let day = days[dateKey] else {
+            return nil
+        }
+        return day.all()
+    }
+}
+
+class Day {
+    let date: Date
+    var records: [Record]
+    
+    init(date: Date, records: [Record]) {
+        self.date = date
+        self.records = records
+    }
+    
+    func main() -> Record {
+        return records.min()!
+    }
+    
+    func all() -> [Record] {
+        records.sort()
+        return records
+    }
+}
