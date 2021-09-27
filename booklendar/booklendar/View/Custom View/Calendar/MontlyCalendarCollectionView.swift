5e36a3f36291926d92b25f4b2b4986daff6683f7
Song Lee <songlish233@gmail.com>
2021-09-27 14:39:41 +0900

    feat: [#9] 월간 캘린더 view 구성

---
+++ booklendar/booklendar/View/Custom View/Calendar/MontlyCalendarCollectionView.swift
@@ -0,0 +1,53 @@
+//
+//  MontlyCalendarCollectionView.swift
+//  booklendar
+//
+//  Created by Song on 2021/09/27.
+//
+
+import UIKit
+
+final class MontlyCalendarCollectionView: UICollectionView {
+    
+    private let borderWitdh: CGFloat = 1
+    
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    private func configure() {
+        setup()
+        registerCells()
+    }
+    
+    private func setup() {
+        backgroundColor = .white
+        showsVerticalScrollIndicator = false
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.borderWidth = borderWitdh
+        layer.borderColor = UIColor.darkGray.cgColor
+    }
+    
+    private func registerCells() {
+        registerContentsCell()
+        registerHeaderCell()
+    }
+    
+    private func registerContentsCell() {
+        let cellId = MontlyCalendarCollectionViewCell.identifier
+        register(MontlyCalendarCollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    private func registerHeaderCell() {
+        let cellId = CalendarHeaderCollectionViewCell.identifier
+        register(CalendarHeaderCollectionViewCell.self,
+                 forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                 withReuseIdentifier: cellId)
+    }
+}
