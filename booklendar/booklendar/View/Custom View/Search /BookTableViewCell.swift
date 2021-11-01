282d8741f262ef6a594f283a34225024e18bef46
Song Lee <songlish233@gmail.com>
2021-11-01 13:41:12 +0900

    feat: [#20] book table cell 디자인 수정

--- booklendar/booklendar/View/Custom View/Search /BookTableViewCell.swift
+++ booklendar/booklendar/View/Custom View/Search /BookTableViewCell.swift
@@ -20,18 +20,19 @@ final class BookTableViewCell: UITableViewCell {
         let label = UILabel()
         label.textAlignment = .left
         label.numberOfLines = 1
-        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     private lazy var infoLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .left
+        label.textColor = .darkGray
+        label.font = UIFont.systemFont(ofSize: 12)
         label.numberOfLines = 1
-        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
+    private lazy var imageWidth: CGFloat = frame.width * 0.16
     private let spacing: CGFloat = 10
     
     required init?(coder: NSCoder) {
@@ -53,28 +54,30 @@ final class BookTableViewCell: UITableViewCell {
         addSubview(coverImageView)
         
         NSLayoutConstraint.activate([
-            coverImageView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.08),
+            coverImageView.widthAnchor.constraint(equalToConstant: imageWidth),
             coverImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
             coverImageView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
-            coverImageView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
             coverImageView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
         ])
     }
     
     private func addLabels() {
-        addSubview(titleLabel)
-        addSubview(infoLabel)
+        let labelStackView = UIStackView(arrangedSubviews: [titleLabel, infoLabel])
+        labelStackView.axis = .vertical
+        labelStackView.spacing = 2
+        labelStackView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(labelStackView)
         
         NSLayoutConstraint.activate([
-            titleLabel.leadingAnchor.constraint(equalTo: coverImageView.trailingAnchor, constant: spacing),
-            titleLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: spacing),
-            infoLabel.leadingAnchor.constraint(equalTo: coverImageView.trailingAnchor, constant: spacing),
-            infoLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: -spacing)
+            labelStackView.leadingAnchor.constraint(equalTo: coverImageView.trailingAnchor, constant: spacing),
+            labelStackView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: spacing),
+            labelStackView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -spacing),
+            labelStackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: spacing)
         ])
     }
     
     func configure(with book: Book) {
         titleLabel.text = book.title
-        infoLabel.text = book.authors.joined(separator: ",") + " / " + book.publisher
+        infoLabel.text = book.authors.joined(separator: ", ") + " / " + book.publisher
     }
 }
