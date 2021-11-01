9aa119d27e60d39c0a069dbbd7e28a9cc8aeb58d
Song Lee <songlish233@gmail.com>
2021-11-01 11:53:04 +0900

    feat: [#20] searh bar, book table view 구성

--- booklendar/booklendar/View/View Controller/SearchViewController.swift
+++ booklendar/booklendar/View/View Controller/SearchViewController.swift
@@ -8,10 +8,46 @@
 import UIKit
 
 final class SearchViewController: UIViewController, ViewModelIncludable {
+    
+    // View
+    private var searchController: UISearchController = {
+        let searchController = UISearchController()
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.automaticallyShowsCancelButton = false
+        return searchController
+    }()
+    
+    private var bookTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .white
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    // View Model
     var viewModel: SearchViewModel?
     typealias ViewModel = SearchViewModel
     
-    override func viewDidLoad() {
+    override func loadView() {
+        super.loadView()
+        configure()
+    }
+    
+    private func configure() {
         view.backgroundColor = .white
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false 
+        addBookTableView()
+    }
+    
+    private func addBookTableView() {
+        view.addSubview(bookTableView)
+        
+        NSLayoutConstraint.activate([
+            bookTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            bookTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bookTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            bookTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
     }
 }
